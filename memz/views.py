37f95e925915bb2e507c011fd3a6ad768ef7f206
mayankprg <mayankrawatprg@gmail.com>
2022-08-24 03:21:08 +0530

    add axios

--- memz/views.py
+++ memz/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
 
 def front(request):
-    context = { }
+    context = { "hi": "hello"}
     return render(request, "index.html", context)
